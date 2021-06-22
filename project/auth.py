8133dfa27fa5ab3525bd1d139a7e419e00e1f016
amir2227 <amir.reza2823@gmail.com>
2021-06-22 22:09:54 +0430

    modified login and signup

--- project/auth.py
+++ project/auth.py
@@ -1,11 +1,27 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, flash
 from flask.globals import request
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user
+from .models import User
+from . import db
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login')
 def login():
+    user_name = request.form.get('user_name')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(user_name=user_name).first()
+
+    if not user and not check_password_hash(user.password, password):
+        flash('Please check your login details and try again')
+        return redirect(url_for('auth.login'))
+    
+    login_user(user)
+
     return render_template('login.html')
 
 
@@ -22,7 +38,16 @@ def signup_post():
     full_name = request.form.get('full_name')
     password = request.form.get('password')
 
-    
+    user = User.query.filter_by(user_name=user_name).first()
+    if user:
+        flash('User name already exists')
+        return redirect(url_for('auth.signup'))
+
+    new_user = User(user_name=user_name, full_name=full_name, password=generate_password_hash(password, method='sha256'))
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for('auth.login'))
 
 @auth.route('/logout')
 def logout():
